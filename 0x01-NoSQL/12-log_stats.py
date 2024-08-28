9555c056960fe50115fb7dca464961fcd2ce06b0
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-28 23:03:31 +0300

    final!

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,15 +5,19 @@ from pymongo import MongoClient
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    t_logs= client.logs.nginx
+    t_logs = client.logs.nginx
     count_logs = t_logs.count_documents({})
+    print(f"{count_logs} logs")
+    print("Methods")
     get_logs = t_logs.count_documents({"method": "GET"})
+    print(f"\tmethod GET: {get_logs}")
     post_logs = t_logs.count_documents({"method": "POST"})
+    print(f"\tmethod POST: {post_logs}")
     put_logs = t_logs.count_documents({"method": "PUT"})
+    print(f"\tmethod PUT: {put_logs}")
     patch_logs = t_logs.count_documents({"method": "PATCH"})
+    print(f"\tmethod PATCH: {patch_logs}")
     delete_logs = t_logs.count_documents({"method": "DELETE"})
+    print(f"\tmethod DELTE: {delete_logs}")
     st_logs = t_logs.count_documents({"method": "GET", "path": "/status"})
-    print("{} logs\n Methods:\n \t method GET: {}\n\t method POST: {}\n\t method"
-           "PUT: {}\n\t method PATCH: {}\n\t method DELETE: {}"
-          "\n {} status check".format(count_logs, get_logs, post_logs, put_logs, patch_logs, 
-                                      delete_logs, st_logs))
+    print(f"{st_logs} status check")
