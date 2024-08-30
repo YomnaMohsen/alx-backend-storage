34efd95a67aef42ec17785b9bb800fe0b9f52783
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-30 22:49:15 +0300

    t 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""redis module"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self) -> None:
+        """initialize redis client"""
+        __redis = redis.Redis()
+        __redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """takes data as arg and store it with key generated
+        by uuid and return key"""
+        key: str = str(uuid.uuid4())
+        self.__redis.set(key, data)
+        return key
