8e9a17904bebf96b02f5f75f6ccc86fcfba28a9e
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-31 22:36:30 +0300

    t 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """redis module"""
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 
@@ -18,3 +18,21 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None):
+        """convert the data back to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value) if value else None
+        else:
+            return value
+
+    def get_str(self, key: str):
+        """automatically parametrize Cache.get with the correct
+        conversion function"""
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: int):
+        """automatically parametrize Cache.get with the
+        correct conversion function"""
+        return self.get(key, lambda d: int(d))
