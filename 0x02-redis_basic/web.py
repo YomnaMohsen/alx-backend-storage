a836b29d235c3223ac47b0c07e8999867e9b5b18
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-09-01 17:43:23 +0300

    fix t 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,13 +9,15 @@ def get_page(url: str) -> str:
     """uses the requests module
     to obtain the HTML content of a particular URL
     and returns it"""
-    key = "count:{url}"
+    key = f"count:{url}"
     r = redis.Redis()
     count = r.incr(key)
     # checks if result already cached
-    cached = r.get("{url}")
+    cached = r.get(f"{url}")
     if cached:
         return cached.decode("utf-8")
     resp = requests.get(url)
-    r.set("{url}", resp, 10)
+    r.set(f"{url}", resp.text, 10)
     return resp.text
+    
+get_page('http://google.com')
