0c7729f135313570cda2b78de36551f11087a442
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-09-01 21:09:32 +0300

    calling fn

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -32,3 +32,6 @@ def get_page(url: str) -> str:
     a particular URL and returns it"""
     resp = requests.get(url)
     return resp.text
+
+if __name__ == "__main__":
+    get_page('http://google.com')
